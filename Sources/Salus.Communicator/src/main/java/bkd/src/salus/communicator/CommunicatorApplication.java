2c518a044d273866b348a2ec8ee1286fca4a2f4f
Matheus Zacarias <74462682+MatheusZacarias657@users.noreply.github.com>
2024-10-02 21:36:18 -0300

    add communicator

---
+++ Sources/Salus.Communicator/src/main/java/bkd/src/salus/communicator/CommunicatorApplication.java
@@ -0,0 +1,13 @@
+package bkd.src.salus.communicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicatorApplication.class, args);
+	}
+
+}
---
+++ Sources/Salus.Communicator/src/test/java/bkd/src/salus/communicator/CommunicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package bkd.src.salus.communicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
